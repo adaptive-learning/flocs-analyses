66314d5c2d4b1b88bdb0a2cbb7ef3028eb598f67
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-12-09 10:07:43 +0100

    Tests for visited set/multiset distance

--- robotanik-experiment/similarity.py
+++ robotanik-experiment/similarity.py
@@ -5,31 +5,77 @@ from robotanik_read import load_problem
 from robotanik_properties import simulate
 
 
-def main():
-    #visited_fields_analysis()
-    pass
+def test():
+    run_doctests()
+    #problem = load_problem(problem_id='639')
+    #programA = problem['attempts'][0]
+    #programB = problem['attempts'][1]
+    #d = visited_sets_distance(problem, programA, programB)
+    #print(d)
 
 
-def visited_multisets_distance(problem, programA, programB):
-    visited_multiset_A = compute_visited_multiset(problem, programA)
-    visited_multiset_B = compute_visited_multiset(problem, programB)
-    distance = jaccard_distance(visited_multiset_A, visited_multiset_B)
-    return distance
+def run_doctests():
+    import doctest
+    doctest.testmod()
 
 
 def visited_sets_distance(problem, programA, programB):
+    """Jaccard distance between sets of visted fields
+
+    >>> problem = load_problem(problem_id='639')
+    >>> programA = [[('_', 'F'), ('_', 'F'), ('_', 'F')], [], [], [], [], []]
+    >>> programB = [[('_', 'L'), ('_', 'F')], [], [], [], []]
+    >>> visited_sets_distance(problem, programA, programB)
+    0.8
+    """
     visited_set_A = compute_visited_set(problem, programA)
     visited_set_B = compute_visited_set(problem, programB)
     distance = jaccard_distance(visited_set_A, visited_set_B)
     return distance
 
 
+def visited_multisets_distance(problem, programA, programB):
+    """Jaccard distance between multisets of visted fields
+
+    >>> problem = load_problem(problem_id='639')
+    >>> programA = [[('_', 'F'), ('_', 'F')], [], [], [], [], []]
+    >>> programB = [[('_', 'L'), ('_', 'F'), ('_', 'L'), ('_', 'L'), ('_', 'F')], [], [], [], []]
+    >>> visited_multisets_distance(problem, programA, programB)
+    0.8
+    """
+    visited_multiset_A = compute_visited_multiset(problem, programA)
+    visited_multiset_B = compute_visited_multiset(problem, programB)
+    distance = jaccard_multisets_distance(visited_multiset_A, visited_multiset_B)
+    return distance
+
+
 def jaccard_distance(setA, setB):
-    """Distance between two sets or multisets (counters)
+    """Distance between two sets
+
+    >>> jaccard_distance({1, 2}, {1, 2})
+    0.0
+    >>> jaccard_distance(set(), set())
+    0.0
+    >>> jaccard_distance({1, 2}, {3, 4})
+    1.0
+    >>> jaccard_distance({1, 2, 3}, {1, 2, 4, 5})
+    0.6
     """
     intersection_size = len(setA & setB)
     union_size = len(setA | setB)
-    distance = (1 - intersection_size / union_size) if union_size > 0 else 0
+    distance = (1 - intersection_size / union_size) if union_size > 0 else 0.
+    return distance
+
+
+def jaccard_multisets_distance(multisetA, multisetB):
+    """Distance between two multisets (counters)
+
+    >>> jaccard_multisets_distance(Counter('aaab'), Counter('aac'))
+    0.6
+    """
+    intersection_size = sum((multisetA & multisetB).values())
+    union_size = sum((multisetA | multisetB).values())
+    distance = (1 - intersection_size / union_size) if union_size > 0 else 0.
     return distance
 
 
@@ -70,4 +116,4 @@ def compute_visited_multiset(problem, attempt):
 
 
 if __name__ == "__main__":
-    main()
+    test()
