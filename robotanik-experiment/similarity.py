4c7db87769ab4914fce3531a7df85ee25ed15437
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-12-09 13:58:20 +0100

    Implement basic edit distance

--- robotanik-experiment/similarity.py
+++ robotanik-experiment/similarity.py
@@ -1,4 +1,5 @@
-from collections import Counter
+from collections import Counter, defaultdict
+import math
 import numpy as np
 import pylab as plt
 from robotanik_read import load_problem
@@ -22,8 +23,74 @@ def run_doctests():
 
 def edit_distance(programA, programB):
     """Cost of edits to transform one program into another
+
+    >>> programA = parse_roboprogram('_F_F_F_F')
+    >>> programB = parse_roboprogram('_F')
+    >>> edit_distance(programA, programB)
+    6
     """
-    pass
+    codeA = program_to_code(programA)
+    codeB = program_to_code(programB)
+    # TODO: canonization (may be either applied as a pre-step or as a part of
+    # edit distance computing (edits with 0 penalties).
+    # TODO: normalization to squeeze the distance between 0 and 1 (?)
+    return edit_distance_steps(codeA, codeB)
+
+
+def program_to_code(program):
+    """
+    >>> program = parse_roboprogram('_FbR_1|_L')
+    >>> program_to_code(program)
+    '_FbR_1|_L'
+    """
+    commands_codes = [[''.join(command_tuple) for command_tuple in function]
+                     for function  in program]
+    functions_codes = [''.join(function) for function in commands_codes]
+    code = '|'.join(functions_codes)
+    return code
+
+
+def edit_distance_steps(codeA, codeB):
+    """Cost of edits to transform one code into another
+
+    Same codes have distance 0
+    >>> edit_distance_steps('_F', '_F')
+    0
+
+    Change of a letter has cost 1
+    >>> edit_distance_steps('_F', '_R')
+    1
+
+    Adding or removing a letter has cost 1
+    >>> edit_distance_steps('', '_F')
+    2
+
+    More complex example (moving one block (bL) and adding color)
+    >>> edit_distance_steps('_F_RbL_1', 'bL_F_Rr1')
+    5
+    """
+    distances = defaultdict(lambda: math.inf)
+    distances[(0,0)] = 0
+    for i, a in enumerate(' ' + codeA):
+        for j, b in enumerate(' ' + codeB):
+            if (i, j) == (0, 0):
+                continue
+            options = [
+                distances[(i-1,j)] + adding_cost(a),
+                distances[(i,j-1)] + adding_cost(b),
+                distances[(i-1,j-1)] + change_cost(a, b),
+            ]
+            distances[(i,j)] = min(options)
+    total_distance = distances[(len(codeA), len(codeB))]
+    return total_distance
+
+
+def adding_cost(letter):
+    return 1
+
+
+def change_cost(a, b):
+    return 1 - int(a == b)
 
 
 def world_effect_distance(problem, programA, programB):
@@ -39,7 +106,6 @@ def world_effect_distance(problem, programA, programB):
 
 def eaten_flowers_distance(problem, programA, programB):
     """Distance between the sets of eaten flowers
-    # TODO: try replace tuples by parse_roboprogram
 
     >>> problem = load_problem(problem_id='659')  # Tree of flowers
     >>> programA = parse_roboprogram('_F_F_F_F')
